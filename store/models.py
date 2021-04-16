d191018752c040d031da43d7ae41e5f2adb42e61
himanshuharlalka <himanshuharlalka@gmail.com>
2021-04-17 00:59:54 +0530

    added COD

--- store/models.py
+++ store/models.py
@@ -90,6 +90,7 @@ class ShippingAddress(models.Model):
     state = models.CharField(max_length=200, null=False)
     zipcode = models.CharField(max_length=200, null=False)
     date_added = models.DateTimeField(auto_now_add=True)
+    cod = models.BooleanField(default=False)
 
     def __str__(self):
         return self.address

43a421022cd832bcd041ea00666b5dd443fd760f
Chirag Jagad <chiragjagad08@gmail.com>
2021-04-18 17:44:11 +0530

    order status

--- store/models.py
+++ store/models.py
@@ -43,6 +43,7 @@ class Order(models.Model):
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
+    status = models.CharField(max_length=100, default="Processing")
 
     def __str__(self):
         return str(self.id)

513998b0dc5acc570a015b0df84f43c3a6f24a4e
Chirag Jagad <chiragjagad08@gmail.com>
2021-04-23 15:42:36 +0530

    ui

--- store/views.py
+++ store/views.py
@@ -260,7 +260,8 @@ def checkout(request):
         order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
 
-    context = {'items': items, 'order': order, 'cartItems': cartItems}
+    context = {'items': items, 'order': order,
+               'cartItems': cartItems, 'customer': customer}
     return render(request, 'store/checkout.html', context)
 
 
